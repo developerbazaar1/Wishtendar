132b8178f7033e8ccc6d16ade4d2c20558f09804
Alok soni <aloksoni201298@gmail.com>
2023-12-06 17:49:16 +0530

    create and manage pages

--- src/Pages/CartFighter.jsx
+++ src/Pages/CartFighter.jsx
@@ -44,6 +44,7 @@ const CartFighter = () => {
       </section>
       {/* section for fighter cart  */}
       <section className="fighter-cart-item">
+        {/* fighter info */}
         <div className="row justify-content-center">
           <div className="col-md-11 col-sm-12 col-xs-12 col-lg-11">
             <div className="cart-detail">
@@ -58,9 +59,7 @@ const CartFighter = () => {
             </div>
           </div>
         </div>
-      </section>
-      {/* cart product table */}
-      <section className="cart-product">
+        {/* fighter cart data */}
         <div className="row justify-content-center">
           <div className="col-md-11 col-sm-12 col-lg-11 col-xs-12">
             <div className="cart-table">
@@ -210,7 +209,139 @@ const CartFighter = () => {
             </div>
           </div>
         </div>
+        {/* :: cart items for many personaliies */}
+        <div className="row justify-content-center">
+          <div className="col-md-11 col-sm-12 col-xs-12 col-lg-11">
+            <div className="cart-detail">
+              <div className="fighter-name">
+                <h6 className="user-call">
+                  Rolly Rich <span className="Goal">goal</span>
+                </h6>
+                <p className="user-recall">
+                  You are about to contribute to Rolland Goals
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* cart detail for other */}
+        <div className="row justify-content-center">
+          <div className="col-md-11 col-sm-12 col-lg-11 col-xs-12">
+            <div className="cart-table">
+              <Table responsive className="cart-product-table">
+                <thead className="cart-product-head">
+                  <tr>
+                    <th className="text-left">Goal</th>
+                    <th>Type/Qty</th>
+                    <th>Amount</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody className="cart-product-body">
+                  {/* table row 01 */}
+                  <tr>
+                    <td>
+                      <div className="product-image text-left">
+                        <img
+                          // loading="lazy"
+                          src="https://cdn.builder.io/api/v1/image/assets/TEMP/d5adabbe-1b4b-4813-be62-fe95030c855b?apiKey=a05c6109e97c4bde98e757ca99d37c45&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/d5adabbe-1b4b-4813-be62-fe95030c855b?apiKey=a05c6109e97c4bde98e757ca99d37c45&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/d5adabbe-1b4b-4813-be62-fe95030c855b?apiKey=a05c6109e97c4bde98e757ca99d37c45&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/d5adabbe-1b4b-4813-be62-fe95030c855b?apiKey=a05c6109e97c4bde98e757ca99d37c45&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/d5adabbe-1b4b-4813-be62-fe95030c855b?apiKey=a05c6109e97c4bde98e757ca99d37c45&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/d5adabbe-1b4b-4813-be62-fe95030c855b?apiKey=a05c6109e97c4bde98e757ca99d37c45&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/d5adabbe-1b4b-4813-be62-fe95030c855b?apiKey=a05c6109e97c4bde98e757ca99d37c45&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/d5adabbe-1b4b-4813-be62-fe95030c855b?apiKey=a05c6109e97c4bde98e757ca99d37c45&"
+                          className="product-thumbnail"
+                          alt="profile"
+                        />
+                        <div className="image-tittle">Gloves</div>
+                      </div>
+                    </td>
+                    <td className="own-category t-data"> Crowdfunded</td>
+                    <td className="product-prics t-data">
+                      <small className="est">Est.</small> <span>CA$50.00</span>{" "}
+                      <span className="model mx-1">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="18"
+                          height="18"
+                          viewBox="0 0 21 20"
+                          fill="none"
+                        >
+                          <path
+                            d="M10.667 2.5C11.6519 2.5 12.6272 2.69399 13.5371 3.0709C14.4471 3.44781 15.2739 4.00026 15.9703 4.6967C16.6667 5.39314 17.2192 6.21993 17.5961 7.12987C17.973 8.03982 18.167 9.01509 18.167 10C18.167 10.9849 17.973 11.9602 17.5961 12.8701C17.2192 13.7801 16.6667 14.6069 15.9703 15.3033C15.2739 15.9997 14.4471 16.5522 13.5371 16.9291C12.6272 17.306 11.6519 17.5 10.667 17.5C8.67787 17.5 6.77021 16.7098 5.36369 15.3033C3.95717 13.8968 3.16699 11.9891 3.16699 10C3.16699 8.01088 3.95717 6.10322 5.36369 4.6967C6.77021 3.29018 8.67787 2.5 10.667 2.5ZM10.667 13.125C10.4184 13.125 10.1799 13.2238 10.0041 13.3996C9.82826 13.5754 9.72949 13.8139 9.72949 14.0625C9.72949 14.3111 9.82826 14.5496 10.0041 14.7254C10.1799 14.9012 10.4184 15 10.667 15C10.9156 15 11.1541 14.9012 11.3299 14.7254C11.5057 14.5496 11.6045 14.3111 11.6045 14.0625C11.6045 13.8139 11.5057 13.5754 11.3299 13.3996C11.1541 13.2238 10.9156 13.125 10.667 13.125ZM10.667 5.625C10.004 5.625 9.36807 5.88839 8.89923 6.35723C8.43038 6.82607 8.16699 7.46196 8.16699 8.125C8.16699 8.29076 8.23284 8.44973 8.35005 8.56694C8.46726 8.68415 8.62623 8.75 8.79199 8.75C8.95775 8.75 9.11672 8.68415 9.23393 8.56694C9.35114 8.44973 9.41699 8.29076 9.41699 8.125C9.41699 7.79348 9.54869 7.47554 9.78311 7.24112C10.0175 7.0067 10.3355 6.875 10.667 6.875C10.9985 6.875 11.3165 7.0067 11.5509 7.24112C11.7853 7.47554 11.917 7.79348 11.917 8.125C11.917 8.5875 11.8132 8.85 11.4595 9.2475L11.3145 9.40375L10.9845 9.74125C10.307 10.45 10.042 10.96 10.042 11.875C10.042 12.0408 10.1078 12.1997 10.2251 12.3169C10.3423 12.4342 10.5012 12.5 10.667 12.5C10.8328 12.5 10.9917 12.4342 11.1089 12.3169C11.2261 12.1997 11.292 12.0408 11.292 11.875C11.292 11.4125 11.3957 11.15 11.7495 10.7525L11.8945 10.5963L12.2245 10.2587C12.902 9.55 13.167 9.04 13.167 8.125C13.167 7.46196 12.9036 6.82607 12.4348 6.35723C11.9659 5.88839 11.33 5.625 10.667 5.625Z"
+                            fill="#757575"
+                          />
+                        </svg>
+                      </span>
+                    </td>
+                    <td className="remove-product">
+                      <Link to="#" className="remove-prdct">
+                        Remove
+                      </Link>
+                    </td>
+                  </tr>
+                  {/* table row 01 end here */}
+                  {/* table row 02 */}
+                  <tr>
+                    <td>
+                      <div className="product-image text-left">
+                        <img
+                          // loading="lazy"
+                          src="https://cdn.builder.io/api/v1/image/assets/TEMP/8bcf31b9-b219-4261-922d-1a417c00bdee?apiKey=a05c6109e97c4bde98e757ca99d37c45&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/8bcf31b9-b219-4261-922d-1a417c00bdee?apiKey=a05c6109e97c4bde98e757ca99d37c45&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/8bcf31b9-b219-4261-922d-1a417c00bdee?apiKey=a05c6109e97c4bde98e757ca99d37c45&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/8bcf31b9-b219-4261-922d-1a417c00bdee?apiKey=a05c6109e97c4bde98e757ca99d37c45&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/8bcf31b9-b219-4261-922d-1a417c00bdee?apiKey=a05c6109e97c4bde98e757ca99d37c45&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/8bcf31b9-b219-4261-922d-1a417c00bdee?apiKey=a05c6109e97c4bde98e757ca99d37c45&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/8bcf31b9-b219-4261-922d-1a417c00bdee?apiKey=a05c6109e97c4bde98e757ca99d37c45&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/8bcf31b9-b219-4261-922d-1a417c00bdee?apiKey=a05c6109e97c4bde98e757ca99d37c45&"
+                          className="product-thumbnail"
+                          alt="profile"
+                        />
+                        <div className="image-tittle">Daily Coffee</div>
+                      </div>
+                    </td>
+                    <td className="own-category t-data">
+                      {" "}
+                      Subscription{" "}
+                      <span>
+                        <Badge pill bg="light" text="dark">
+                          Daily
+                        </Badge>
+                      </span>
+                    </td>
+                    <td className="product-prics t-data">
+                      <small className="est">Est.</small> <span>CA$50.00</span>{" "}
+                      <span className="model mx-1">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="18"
+                          height="18"
+                          viewBox="0 0 21 20"
+                          fill="none"
+                        >
+                          <path
+                            d="M10.667 2.5C11.6519 2.5 12.6272 2.69399 13.5371 3.0709C14.4471 3.44781 15.2739 4.00026 15.9703 4.6967C16.6667 5.39314 17.2192 6.21993 17.5961 7.12987C17.973 8.03982 18.167 9.01509 18.167 10C18.167 10.9849 17.973 11.9602 17.5961 12.8701C17.2192 13.7801 16.6667 14.6069 15.9703 15.3033C15.2739 15.9997 14.4471 16.5522 13.5371 16.9291C12.6272 17.306 11.6519 17.5 10.667 17.5C8.67787 17.5 6.77021 16.7098 5.36369 15.3033C3.95717 13.8968 3.16699 11.9891 3.16699 10C3.16699 8.01088 3.95717 6.10322 5.36369 4.6967C6.77021 3.29018 8.67787 2.5 10.667 2.5ZM10.667 13.125C10.4184 13.125 10.1799 13.2238 10.0041 13.3996C9.82826 13.5754 9.72949 13.8139 9.72949 14.0625C9.72949 14.3111 9.82826 14.5496 10.0041 14.7254C10.1799 14.9012 10.4184 15 10.667 15C10.9156 15 11.1541 14.9012 11.3299 14.7254C11.5057 14.5496 11.6045 14.3111 11.6045 14.0625C11.6045 13.8139 11.5057 13.5754 11.3299 13.3996C11.1541 13.2238 10.9156 13.125 10.667 13.125ZM10.667 5.625C10.004 5.625 9.36807 5.88839 8.89923 6.35723C8.43038 6.82607 8.16699 7.46196 8.16699 8.125C8.16699 8.29076 8.23284 8.44973 8.35005 8.56694C8.46726 8.68415 8.62623 8.75 8.79199 8.75C8.95775 8.75 9.11672 8.68415 9.23393 8.56694C9.35114 8.44973 9.41699 8.29076 9.41699 8.125C9.41699 7.79348 9.54869 7.47554 9.78311 7.24112C10.0175 7.0067 10.3355 6.875 10.667 6.875C10.9985 6.875 11.3165 7.0067 11.5509 7.24112C11.7853 7.47554 11.917 7.79348 11.917 8.125C11.917 8.5875 11.8132 8.85 11.4595 9.2475L11.3145 9.40375L10.9845 9.74125C10.307 10.45 10.042 10.96 10.042 11.875C10.042 12.0408 10.1078 12.1997 10.2251 12.3169C10.3423 12.4342 10.5012 12.5 10.667 12.5C10.8328 12.5 10.9917 12.4342 11.1089 12.3169C11.2261 12.1997 11.292 12.0408 11.292 11.875C11.292 11.4125 11.3957 11.15 11.7495 10.7525L11.8945 10.5963L12.2245 10.2587C12.902 9.55 13.167 9.04 13.167 8.125C13.167 7.46196 12.9036 6.82607 12.4348 6.35723C11.9659 5.88839 11.33 5.625 10.667 5.625Z"
+                            fill="#757575"
+                          />
+                        </svg>
+                      </span>
+                    </td>
+                    <td className="remove-product">
+                      <Link to="#" className="remove-prdct">
+                        Remove
+                      </Link>
+                    </td>
+                  </tr>
+                  {/* table row 02 end here */}
+                </tbody>
+              </Table>
+            </div>
+          </div>
+        </div>
+        {/* for payment details*/}
+
+        {/* for payment details end */}
       </section>
+      {/* cart product table */}
+      {/* :: section for add message  */}
+      <div className="row justify-content-center mt-2 mb-4 message-tab">
+        <div className="col-md-11 col-lg-11 col-sm-12 col-xs-12">
+          <div className="form-group">
+            <label className="form-head" htmlFor="message-area">Message</label>
+            <textarea className="form-control" placeholder="Write your message here" id="exampleTextarea" rows="6"></textarea>
+          </div>
+        </div>
+      </div>
+      {/* :: section for add message end here */}
       {/* section for  a cart terms and condition  */}
       <section className="cart-terms">
         <div className="row justify-content-center">
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,13 @@ const Footer = () => {
     <>
       <div className="footer-container">
         <ul className="footer-ul footer-fonts">
-          <li><Link to="/about" className="link-text">About</Link> </li>
-          <li> <Link to="/helpcenter" className="link-text">Help Center</Link> </li>
-          <li> <Link to="/terms-of-services" className="link-text">Terms of Service</Link> </li>
-          <li> <Link to="/privacypolicy" className="link-text">Privacy Policy</Link></li>
-          <li> <Link to="/cookie-policy" className="link-text">Cookie Policy</Link></li>
-          <li> <Link to="/how-it-works" className="link-text">How its works</Link> </li>
-          <li> <Link to="/faq" className="link-text">FAQ</Link></li>
+          <li><Link to="/about" type="button" className="link-text">About</Link> </li>
+          <li> <Link to="/helpcenter" type="button" className="link-text">Help Center</Link> </li>
+          <li> <Link to="/terms-of-services" type="button" className="link-text">Terms of Service</Link> </li>
+          <li> <Link to="/privacypolicy" type="button" className="link-text">Privacy Policy</Link></li>
+          <li> <Link to="/cookie-policy" type="button" className="link-text">Cookie Policy</Link></li>
+          <li> <Link to="/how-it-works" type="button" className="link-text">How its works</Link> </li>
+          <li> <Link to="/faq" type="button" className="link-text">FAQ</Link></li>
           <li><span className="link-text">© 2021 Fight Companion LLC.</span></li>
           {/* <li>How it works</li> */}
         </ul>
